10c3ce5658ea2bad7169c6cc90e95c83b8a1ac78
Vaibhav Muchandi <vaibhavmm@hotmail.com>
2023-11-13 20:26:31 +0530

    add: tag

--- apps/web/components/NavBar.tsx
+++ apps/web/components/NavBar.tsx
@@ -2,7 +2,7 @@ const NavBar = () => {
     return (
         <nav className="bg-black text-white py-4">
             <div className="container mx-auto px-4 flex flex-col justify-center items-center">
-                <h1 className="text-xl font-bold">GPT Assemblage</h1>
+                <h1 className="text-xl font-bold">GPT Assemblage: Custom GPTs Directory</h1>
             </div>
         </nav>
     );
